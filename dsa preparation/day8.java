e71d7271e0f82ec72d919505c0ae44adaa582e7e
Mohd Ali Khan <2002mohdali@gmail.com>
2025-10-18 23:59:44 +0530

    new

--- dsa preparation/day8.java
+++ dsa preparation/day8.java
@@ -71,4 +71,23 @@ class Solution {
         }
         return xor ^ n;
     }
+
+}
+
+
+
+// second solution
+
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length - 1;i++){
+            if(nums[i] + 1 != nums[i+1]){
+                return nums[i] +1;
+            }
+        }
+        if(nums[0] != 0) return 0;
+        return nums[nums.length - 1]+1;
+    }
 }
